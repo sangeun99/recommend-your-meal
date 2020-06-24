faabd98d81efe473ebf0fea081a4ad7a8d81b149
sangeun <sangeun99@khu.ac.kr>
2020-06-25 02:53:16 +0900

    불필요한 파일 삭제

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
-const KAKAOKEY = '6bf1af821e72e1795ed820a6fa553448';
 const request = require('request');
 const KAKAOURL = 'https://dapi.kakao.com/v2/local/search/keyword.json'
 const TARGET_URL = 'https://api.line.me/v2/bot/message/reply'
-const TOKEN = 'dNIqjRizz4FokBqT/MaquXsb1qb+SCxlRNfJOjmK9yu91dSFpPWFDzDvZXWSR5I7pDEoI1F+v+VQktU+sLwoy8yCE4iX/8RswH2w6NcPO9MxmtlPocWqKfaoGjPFjIS/ehisf3bLKFlWQEpJdPkKLgdB04t89/1O/w1cDnyilFU='
+const WEATHERURL = 'https://api.openweathermap.org/data/2.5/weather'
+const TOKEN = 'tdMyZeVtg7OY2v5C9rRC67mxNbOh7SFCxA9dor29QFOh2bmaVDlYRTKMAv0sojmmyQXY4eYzHZKoME5LraY/TPidg+GjhBWw1gYK9gitMV13N21Bw1h0k48pJwfdz6+r4LVrrZm37ggW8kSW6PWW1wdB04t89/1O/w1cDnyilFU='
+const KAKAOKEY = '6bf1af821e72e1795ed820a6fa553448';
+const WEATHERKEY = '3250bfdaf394e55292242e42b0c32eb9';
 const fs = require('fs');
 const path = require('path');
 const HTTPS = require('https');
@@ -18,11 +20,14 @@ var mylocation = { // 기본주소
     "longitude": 127.07758311277773
 };
 
+var c_food = new Array( '냉면', '콩국수', '모밀', '초밥', '샌드위치', '스테이크');
+var h_food = new Array( '마라탕', '칼국수', '짬뽕', '닭볶음탕', '찌개' );
+var r_food = new Array( '파전', '보쌈', '짬뽕', '떡볶이', '찌개' );
+
 app.use(bodyParser.json());
 app.post('/hook', function (req, res) {
 
     var eventObj = req.body.events[0];
-    var source = eventObj.source;
     var message = eventObj.message;
 
     // request log
@@ -35,18 +40,45 @@ app.post('/hook', function (req, res) {
         mylocation = message;
     }
 
-    else if (eventObj.message.text == '내위치'){
+    // else if (! isNaN(eventObj.message.text)){ // 숫자일때
+    // }
+
+    else if (eventObj.message.text == '내위치') {
         quickreply(eventObj.replyToken, eventObj.message.text);
     }
-
-    else {
-        findlocation(eventObj.replyToken, eventObj.message.text);
+    else if (eventObj.message.text == '날씨추천') {
+        getweather(eventObj.replyToken);
     }
 
     res.sendStatus(200);
 });
 
-function findlocation(replyToken, keyword){
+function getweather(replyToken){
+    request.get({
+        url: WEATHERURL+'?lat='+mylocation.latitude+'&lon='+mylocation.longitude+'&appid='+WEATHERKEY+'&units=metric',
+        json : true
+    }, (error, response, body) => {
+        console.log(body);
+        var mainweather = body.weather[0].main;
+        var celcius = parseInt(body.main.temp);
+
+        if (mainweather == 'rain'){
+            randomfood = r_food[Math.floor(Math.random() * r_food.length)];
+            explain = '오늘은 비가 옵니다. 오늘의 추천메뉴는 ' + randomfood + '입니다.';
+        }
+        else if (celcius >= 0){
+            randomfood = c_food[Math.floor(Math.random() * c_food.length)];
+            explain = '오늘은 날씨가 덥습니다. 오늘의 추천메뉴는 ' + randomfood + '입니다.';
+        }
+        else if (celcius < 0){
+            randomfood = h_food[Math.floor(Math.random() * h_food.length)];
+            explain = '오늘은 날씨가 춥습니다. 오늘의 추천메뉴는 ' + randomfood + '입니다.';
+        }
+        findlocation(replyToken, randomfood, explain);
+    });
+}
+
+function findlocation(replyToken, keyword, explain){
 
     var options = {
         url: KAKAOURL,
@@ -56,8 +88,10 @@ function findlocation(replyToken, keyword){
         },
         form : {
             'query' : keyword,
-            'x' : mylocation.latitude,
-            'y': mylocation.longitude,
+            'category_group_code' : 'FD6',
+            'x' : mylocation.longitude,
+            'y' : mylocation.latitude,
+            'radius' : 20000,
             'size' : 1
         },
         json : true
@@ -65,13 +99,15 @@ function findlocation(replyToken, keyword){
 
     request.get(options, function(error,response,body){
         if(!error && response.statusCode == 200) {
-            console.log(body.documents[0]);
-            replyresult(replyToken, body.documents[0]);
+            console.log(body);
+            if (body.documents[0]){
+                replyresult(replyToken, body.documents[0], explain);
+            }
         }
     });
 }
 
-function replyresult(replyToken, result) {
+function replyresult(replyToken, result, explain) {
     request.post(
         {
             url: TARGET_URL,
@@ -81,6 +117,10 @@ function replyresult(replyToken, result) {
             json: {
                 "replyToken":replyToken,
                 "messages":[
+                    {
+                        "type" : "text",
+                        "text" : explain
+                    },
                     {
                         "type": "location",
                         "title": result.place_name,
@@ -107,7 +147,7 @@ function quickreply(replyToken, message) {
                 "messages":[
                     {
                         "type": "text", 
-                        "text": "You can send your location!",
+                        "text": "You can select your location!",
                         "quickReply": {
                           "items": [                            
                             {
